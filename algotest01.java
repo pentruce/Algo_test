2f62b524a20466c273c9feaaffe58ab9d118298a
KOPO_15 <102049642+pentruce@users.noreply.github.com>
2022-05-20 12:46:53 +0900

    Add files via upload

---
+++ algotest01.java
@@ -0,0 +1,19 @@
+package ZZJava_Game;
+
+import java.util.Scanner;
+
+public class algotest01 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+
+		StringBuilder sb = new StringBuilder("Hello");
+		for (int i = 0; i < n; i++) {
+			sb.append(scanner.next()).append(",");
+
+		}
+		System.out.println(sb.substring(0, sb.length() - 1) + ".");
+	}
+}
